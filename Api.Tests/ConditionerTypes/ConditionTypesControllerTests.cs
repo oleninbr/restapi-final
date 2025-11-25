a08d500f81c45e759b35c52f9dc937ec1b3f1c23
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2025-11-25 11:32:37 +0200

    tested conditioner type succesfully

--- Api.Tests/ConditionerTypes/ConditionTypesControllerTests.cs
+++ Api.Tests/ConditionerTypes/ConditionTypesControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using Domain.ConditionerTypes;
+using FluentAssertions;
+using System.Net.Http.Json;
 using Tests.Common;
 using WebAPI.Dtos;
 
@@ -6,10 +8,10 @@ namespace Api.Tests.Integrations.ConditionerTypes
 {
     public class ConditionTypesControllerTests : BaseIntegrationTest, IAsyncLifetime
     {
-        private readonly Domain.ConditionerTypes.ConditionerType _firstTestConditionerType = 
+        private readonly ConditionerType _firstTestConditionerType = 
             Test.Data.ConditionerTypes.ConditionerTypesData.FirstTestType();
 
-        private const string Baseroute = "conditioner-types";
+        private const string BaseRoute = "conditioner-types";
 
         public ConditionTypesControllerTests(IntegrationTestWebFactory factory) : base(factory)
         {
@@ -20,8 +22,9 @@ namespace Api.Tests.Integrations.ConditionerTypes
         {
             //arrange
 
+
             //act
-            var response = await Client.GetAsync(Baseroute);
+            var response = await Client.GetAsync(BaseRoute);
             response.EnsureSuccessStatusCode();
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
@@ -31,6 +34,85 @@ namespace Api.Tests.Integrations.ConditionerTypes
             conditionerTypes.Should().ContainSingle(c => c.Name == _firstTestConditionerType.Name);
         }
 
+
+        [Fact]
+        public async Task ShouldGetConditionerTypeById()
+        {
+            var response = await Client.GetAsync($"{BaseRoute}/{_firstTestConditionerType.Id.Value}");
+            response.EnsureSuccessStatusCode();
+
+            var dto = await response.ToResponseModel<ConditionerTypeDto>();
+            dto.Name.Should().Be(_firstTestConditionerType.Name);
+        }
+
+
+        [Fact]
+        public async Task ShouldReturnNotFoundForWrongId()
+        {
+            var response = await Client.GetAsync($"{BaseRoute}/{Guid.NewGuid()}");
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task ShouldCreateConditionerType()
+        {
+            var request = new CreateConditionerTypeDto("NewTestType");
+
+            var response = await Client.PostAsJsonAsync(BaseRoute, request);
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var created = await response.ToResponseModel<ConditionerTypeDto>();
+            created.Name.Should().Be("NewTestType");
+
+            var dbEntity = Context.ConditionerTypes
+        .First(x => x.Id == new ConditionerTypeId(created.Id));
+
+            dbEntity.Name.Should().Be("NewTestType");
+        }
+
+
+        [Fact]
+        public async Task ShouldUpdateConditionerType()
+        {
+            var request = new UpdateConditionerTypeDto(
+                Id: _firstTestConditionerType.Id.Value,
+                Name: "UpdatedName"
+            );
+
+            var response = await Client.PutAsJsonAsync(BaseRoute, request);
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var updated = Context.ConditionerTypes
+                .First(x => x.Id.Equals(_firstTestConditionerType.Id));
+
+            updated.Name.Should().Be("UpdatedName");
+        }
+
+
+
+        [Fact]
+        public async Task ShouldReturnNotFoundWhenUpdatingMissingType()
+        {
+            var request = new UpdateConditionerTypeDto(
+                Id: Guid.NewGuid(),
+                Name: "DoesNotMatter"
+            );
+
+            var response = await Client.PutAsJsonAsync(BaseRoute, request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+
+        [Fact]
+        public async Task ShouldDeleteConditionerType()
+        {
+            var response = await Client.DeleteAsync($"{BaseRoute}/{_firstTestConditionerType.Id.Value}");
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            Context.ConditionerTypes.Any(x => x.Id == _firstTestConditionerType.Id)
+                .Should().BeFalse();
+        }
 
         public async Task InitializeAsync()
         {
