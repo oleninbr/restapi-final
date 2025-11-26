cd27cfab8be384baa2447dd9b5d310d5babb9a15
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2025-11-26 13:27:11 +0200

    tested manufacturers

---
+++ Api.Tests/Manufacturers/ManufacturersControllerTests.cs
@@ -0,0 +1,124 @@
+﻿using Domain.ConditionerTypes;
+using Domain.Manufacturers;
+using FluentAssertions;
+using System.Net.Http.Json;
+using Tests.Common;
+using WebAPI.Dtos;
+
+namespace Api.Tests.Integrations.Manufacturers
+{
+    public class ManufacturersControllerTests : BaseIntegrationTest, IAsyncLifetime
+    {
+        private readonly Manufacturer _firstTestManufacturer =
+            Test.Data.Manufacturers.ManufacturersData.FirstTestManufacturer();
+        private const string BaseRoute = "manufacturers";
+
+        public ManufacturersControllerTests(IntegrationTestWebFactory factory) : base(factory)
+        {
+        }
+
+
+        [Fact]
+        public async Task ShouldGetAllManufacturers()
+        {
+            // act
+            var response = await Client.GetAsync(BaseRoute);
+            response.EnsureSuccessStatusCode();
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var manufacturers = await response.ToResponseModel<List<ManufacturerDto>>();
+            manufacturers.Should().ContainSingle(m => m.Name == _firstTestManufacturer.Name);
+        }
+
+        [Fact]
+        public async Task ShouldGetManufacturerById()
+        {
+            var response = await Client.GetAsync($"{BaseRoute}/{_firstTestManufacturer.Id.Value}");
+            response.EnsureSuccessStatusCode();
+            var dto = await response.ToResponseModel<ManufacturerDto>();
+            dto.Name.Should().Be(_firstTestManufacturer.Name);
+        }
+
+
+        [Fact]
+        public async Task ShouldReturnNotFoundForWrongId()
+        {
+            var response = await Client.GetAsync($"{BaseRoute}/{Guid.NewGuid()}");
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task ShouldCreateManufacturer()
+        {
+            var request = new CreateManufacturerDto("NewTestType", "NewTestCountry");
+
+            var response = await Client.PostAsJsonAsync(BaseRoute, request);
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var created = await response.ToResponseModel<ManufacturerDto>();
+            created.Name.Should().Be("NewTestType");
+
+            var dbEntity = Context.Manufacturers
+        .First(x => x.Id == new ManufacturerId(created.Id));
+
+            dbEntity.Name.Should().Be("NewTestType");
+        }
+
+        [Fact]
+        public async Task ShouldUpdateManufacturer()
+        {
+            var request = new UpdateManufacturerDto(
+                Id: _firstTestManufacturer.Id.Value,
+                Name: "UpdatedName",
+                Country: "UpdatedCountry"
+            );
+
+            var response = await Client.PutAsJsonAsync(BaseRoute, request);
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var updated = Context.Manufacturers
+                .First(x => x.Id.Equals(_firstTestManufacturer.Id));
+
+            updated.Name.Should().Be("UpdatedName");
+        }
+
+        [Fact]
+        public async Task ShouldReturnBadRequestForDuplicateManufacturerName()
+        {
+            var request = new CreateManufacturerDto(_firstTestManufacturer.Name, "SomeCountry");
+            var response = await Client.PostAsJsonAsync(BaseRoute, request);
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task ShouldReturnBadRequestForShortManufacturerName()
+        {
+            var request = new CreateManufacturerDto("A", "SomeCountry");
+            var response = await Client.PostAsJsonAsync(BaseRoute, request);
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task ShouldDeleteManufacturer()
+        {
+            var response = await Client.DeleteAsync($"{BaseRoute}/{_firstTestManufacturer.Id.Value}");
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            Context.Manufacturers.Any(x => x.Id == _firstTestManufacturer.Id)
+                .Should().BeFalse();
+        }
+
+
+        public async Task InitializeAsync()
+        {
+            await Context.Manufacturers.AddAsync(_firstTestManufacturer);
+            await SaveChangesAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            Context.Manufacturers.RemoveRange(Context.Manufacturers);
+            await SaveChangesAsync();
+        }
+    }
+}
--- src/WebAPI/Controllers/ManufacturersController.cs
+++ src/WebAPI/Controllers/ManufacturersController.cs
@@ -4,6 +4,7 @@ using Application.Manufacturers.Commands;
 using Application.Common.Interfaces.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services.Abstract;
 
 namespace WebAPI.Controllers;
 
@@ -11,6 +12,7 @@ namespace WebAPI.Controllers;
 [Route("manufacturers")]
 public class ManufacturersController(
     IManufacturerQueries queries,
+    IManufacturersControllerService controllerService,
     ISender sender) : ControllerBase
 {
     [HttpGet]
@@ -18,6 +20,18 @@ public class ManufacturersController(
     {
         var entities = await queries.GetAllAsync(cancellationToken);
         return entities.Select(ManufacturerDto.FromDomainModel).ToList();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ManufacturerDto>> Get(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken)
+    {
+        var entity = await controllerService.Get(id, cancellationToken);
+
+        return entity.Match<ActionResult<ManufacturerDto>>(
+            e => e,
+            () => NotFound());
     }
 
     [HttpPost]
